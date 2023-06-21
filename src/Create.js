3b83c6e5070987ac38ece1faa075acc7a25e5005
jasdhiratrevature <jasdhir.singh@revature.com>
2023-06-21 18:00:15 +0530

    29-POST_Request

--- src/Create.js
+++ src/Create.js
@@ -7,8 +7,13 @@ function Create() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-
-    console.log(blog);
+    fetch('http://localhost:3030/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+    })
   }
   return (
     <div className="create">
